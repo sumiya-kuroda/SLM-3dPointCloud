dabab2d7d1ef46827b06e536c02e1804532118ec
Sumiya <26479377+sumiya-kuroda@users.noreply.github.com>
2024-07-08 11:36:41 +0100

    Update `example.py` for 1035nm usage

--- example.py
+++ example.py
@@ -26,16 +26,16 @@ def rotation_matrix(a, b, g):
 slm_to_pupil_magnification = 5.0/3.0
 
 # wavelength of the light source, in nanometers
-wavelength_nm=800
+wavelength_nm=1035
 
 # slm pixel dimension, in micrometers
-slm_pixel_pitch_um = 9.2
+slm_pixel_pitch_um = 17
 
 # screen number of the SLM
 screenID = 1
 
 # Radius of the gaussian laser beam at the SLM surface, in millimeters. Set to None to consider uniform intensity
-beam_radius_slm_mm = 6.0
+beam_radius_slm_mm = None
 
 # focal length of the objective lens, in millimeters (equal to the tube lens focal length of the objective manufacturer,
 # divided by the objective magnification)
@@ -48,14 +48,14 @@ lut_limits=[0,255]
 SLM = slm_3dpointcloud.SlmControl(wavelength_nm,
                                   slm_to_pupil_magnification*slm_pixel_pitch_um,
                                   focal,
-                                  slm_to_pupil_magnification*beam_radius_slm_mm,
+                                  None,
                                   screenID,
                                   None, #use [x_offset,y_offset,diameter] instead of none to define the pupil in a
                                         #subregion of the slm surface. Sizes must be in pixels.
                                   lut_limits)
 
 # Define coordinates for a regular grid of spots
-spots_coords, ints = grid_parameters(10, 300.0)
+spots_coords, ints = grid_parameters(2, 250)
 
 # Test the RS performance
 performance = SLM.rs(spots_coords, ints, get_perf = True)
@@ -65,6 +65,8 @@ print("Efficiency: "+str(performance["Efficiency"]))
 print("Uniformity: "+str(performance["Uniformity"]))
 print("Variance: "+str(performance["Variance"]))
 print("\n")
+phase_arr = SLM.get_phase()
+print(phase_arr.shape)
 
 # Test the WGS performance
 performance = SLM.wgs(spots_coords, ints, 10, get_perf = True)
@@ -84,50 +86,50 @@ print("Uniformity: "+str(performance["Uniformity"]))
 print("Variance: "+str(performance["Variance"]))
 print("\n")
 
-# Show a live rotating grid. get_perf is set to false for maximum live performance
+# # Show a live rotating grid. get_perf is set to false for maximum live performance
 
-# Define rotation rates in pitch, yaw and roll
-rotation_rates_rad_per_s = numpy.array([1.0, 1.5, 2.0])
+# # Define rotation rates in pitch, yaw and roll
+# rotation_rates_rad_per_s = numpy.array([1.0, 1.5, 2.0])
 
-#create an empty array with the rotated coordinates
-rot_coordinates=numpy.zeros_like(spots_coords)
+# #create an empty array with the rotated coordinates
+# rot_coordinates=numpy.zeros_like(spots_coords)
 
-print("Live grid rotation, RS algorithm")
-t_0 = time.perf_counter()
-while time.perf_counter()-t_0 < 10.0:
-    t = time.perf_counter()-t_0
-    angles = rotation_rates_rad_per_s*t
-    rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
-    for i in range(spots_coords.shape[0]):
-        rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
-    SLM.rs(rot_coordinates, ints)
+# print("Live grid rotation, RS algorithm")
+# t_0 = time.perf_counter()
+# while time.perf_counter()-t_0 < 10.0:
+#     t = time.perf_counter()-t_0
+#     angles = rotation_rates_rad_per_s*t
+#     rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
+#     for i in range(spots_coords.shape[0]):
+#         rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
+#     SLM.rs(rot_coordinates, ints)
 
-print("Live grid rotation, WGS algorithm, 10 iterations")
-t_0 = time.perf_counter()
-while time.perf_counter()-t_0 < 10.0:
-    t = time.perf_counter()-t_0
-    angles = rotation_rates_rad_per_s*t
-    rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
-    for i in range(spots_coords.shape[0]):
-        rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
-    SLM.wgs(rot_coordinates, ints, 10)
+# print("Live grid rotation, WGS algorithm, 10 iterations")
+# t_0 = time.perf_counter()
+# while time.perf_counter()-t_0 < 10.0:
+#     t = time.perf_counter()-t_0
+#     angles = rotation_rates_rad_per_s*t
+#     rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
+#     for i in range(spots_coords.shape[0]):
+#         rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
+#     SLM.wgs(rot_coordinates, ints, 10)
 
-print("Live grid rotation, WGS algorithm, 1 iteration")
-t_0 = time.perf_counter()
-while time.perf_counter()-t_0 < 10.0:
-    t = time.perf_counter()-t_0
-    angles = rotation_rates_rad_per_s*t
-    rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
-    for i in range(spots_coords.shape[0]):
-        rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
-    SLM.wgs(rot_coordinates, ints, 1)
+# print("Live grid rotation, WGS algorithm, 1 iteration")
+# t_0 = time.perf_counter()
+# while time.perf_counter()-t_0 < 10.0:
+#     t = time.perf_counter()-t_0
+#     angles = rotation_rates_rad_per_s*t
+#     rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
+#     for i in range(spots_coords.shape[0]):
+#         rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
+#     SLM.wgs(rot_coordinates, ints, 1)
 
-print("Live grid rotation, CS-WGS algorithm, 10 iterations, 16x compression")
-t_0 = time.perf_counter()
-while time.perf_counter()-t_0 < 10.0:
-    t = time.perf_counter()-t_0
-    angles = rotation_rates_rad_per_s*t
-    rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
-    for i in range(spots_coords.shape[0]):
-        rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
-    SLM.cs_wgs(rot_coordinates, ints, 10, 1/16.0)
+# print("Live grid rotation, CS-WGS algorithm, 10 iterations, 16x compression")
+# t_0 = time.perf_counter()
+# while time.perf_counter()-t_0 < 10.0:
+#     t = time.perf_counter()-t_0
+#     angles = rotation_rates_rad_per_s*t
+#     rot_mat = rotation_matrix(angles[0], angles[1], angles[2])
+#     for i in range(spots_coords.shape[0]):
+#         rot_coordinates[i, :] = numpy.dot(rot_mat, spots_coords[i, :])
+#     SLM.cs_wgs(rot_coordinates, ints, 10, 1/16.0)
